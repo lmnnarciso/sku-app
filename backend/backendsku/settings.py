509914389dbe7050864dbf1f2a3ae27c0cb09213
Leandro Miguel Narciso <lmnnarciso14@gmail.com>
2019-10-04 02:14:37 +0800

    Added core and user app, added user model and authentication, added login authentication, added testing for models and login

--- backend/backendsku/settings.py
+++ backend/backendsku/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'core',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
